c4d7b8e3d2bc99599b67ccb0caf173b72b6fb0d5
omaradavid <davidomara256@gmail.com>
2026-04-08 13:25:12 +0300

    Fix bcryptjs typing for production build

--- types/bcryptjs.d.ts
+++ types/bcryptjs.d.ts
@@ -1,4 +1,11 @@
-import bcrypt from "bcryptjs";
+declare module "bcryptjs" {
+  export function hash(data: string, saltOrRounds: string | number): Promise<string>;
+  export function compare(data: string, encrypted: string): Promise<boolean>;
 
-const hashed = await bcrypt.hash(password, 10);
-const ok = await bcrypt.compare(password, hashed);
+  const bcrypt: {
+    hash: typeof hash;
+    compare: typeof compare;
+  };
+
+  export default bcrypt;
+}
