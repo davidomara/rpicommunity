588c398da5dd0bc84c3dcfb593fdf3ec6520dc35
RPIC Deploy <deploy@local>
2026-04-17 15:59:57 +0300

    start cmd

---
+++ start-next.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd /d C:\apps\rpic-community-app || exit /b 1
+robocopy .next\static .next\standalone\.next\static /E >nul
+robocopy public .next\standalone\public /E >nul
+set PATH=C:\Program Files\nodejs;%PATH%
+"C:\Program Files\nodejs\node.exe" .next\standalone\server.js >> C:\apps\rpic-community-app\next-start.log 2>&1
