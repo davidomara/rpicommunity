f52db1a3852f118373f5eed0078d405ff942ef5a
omaradavid <davidomara256@gmail.com>
2026-04-22 13:58:45 +0300

    comments test

--- start-next.cmd
+++ start-next.cmd
@@ -5,3 +5,7 @@ set PATH=C:\Program Files\nodejs;%PATH%
 set PORT=3000
 echo Starting app at %date% %time%>> C:\apps\rpic-community-app\next-start.log
 "C:\Program Files\nodejs\node.exe" server.js >> C:\apps\rpic-community-app\next-start.log 2>&1
+
+@rem schtasks /create /tn "RPIC WWW" /sc onstart /ru "WIN-PBCMT0QQ9B9\svc_gitdeploy" /rp * /rl highest /tr "C:\apps\rpic-community-app\start.cmd" /f
+@rem schtasks /run /tn "RPIC WWW"
+@rem schtasks /query /tn "RPIC WWW" /fo list /v
