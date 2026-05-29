9f03e4c8aa4d4f0aa6091dc2b455de99e3006bbc
omaradavid <davidomara256@gmail.com>
2026-05-29 14:54:20 +0300

    refactor: remove basePath and custom redirects from Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  basePath: "/rpicommunity",
   experimental: {
     serverActions: {
-      bodySizeLimit: "8mb",
-      allowedOrigins: [
-        "10.20.70.138",
-        "10.20.70.138:80",
-        "154.72.204.131",
-        "154.72.204.131:8087"
-      ]
+      bodySizeLimit: "8mb"
     }
-  },
-  async redirects() {
-    return [
-      {
-        source: "/meta.json",
-        destination: "/rpicommunity/meta.json",
-        permanent: false,
-        basePath: false
-      },
-      {
-        source: "/favicon.svg",
-        destination: "/rpicommunity/favicon.svg",
-        permanent: false,
-        basePath: false
-      },
-      {
-        source: "/favicon.ico",
-        destination: "/rpicommunity/favicon.svg",
-        permanent: false,
-        basePath: false
-      }
-    ];
   }
 };
 
